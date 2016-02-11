f8ea74202909f0fe173a8a09ae2b6b5f6c031bd5
JIAN_J <JIAN_J@235700LT-DFSMS.ad.psb.bls.gov>
2016-02-11 17:28:15 -0500

    add sikuli in the framework

--- autotest/src/test/java/com/laus/autotest/CukeSteps.java
+++ autotest/src/test/java/com/laus/autotest/CukeSteps.java
@@ -3,6 +3,8 @@ package com.laus.autotest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Screen;
 
 import cucumber.api.java.en.Given;
 
@@ -11,8 +13,18 @@ public class CukeSteps {
 	@Given("i test cucumber")
 	public void testCuke() throws Throwable{
 		System.out.println("test cucumber complete");
-		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new ChromeDriver();
 		//WebDriver driver = new InternetExplorerDriver();
-		driver.get("http://www.google.com");
+		//driver.get("http://www.google.com");
+		Screen s = new Screen();
+        try{
+                s.click("src/test/images/chrome.png");
+                //s.wait("imgs/spotlight-input.png");
+               // s.click();
+                s.write("hello world#ENTER.");
+        }
+        catch(FindFailed e){
+                e.printStackTrace();
+        }
 	}
 }
